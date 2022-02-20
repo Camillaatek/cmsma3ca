def7bd973883aa5e287d1ad75b7b04872509ed67
Camillaatek <69843303+Camillaatek@users.noreply.github.com>
2022-02-20 22:32:47 +0100

    styled cards

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import { ANIMALS_URL, BASE_URL, POPULATE } from './utils/api';
+import { ITEMS_URL, BASE_URL, POPULATE } from './utils/api';
+import './App.css'
 
 const App = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     axios
-      .get(BASE_URL + ANIMALS_URL + POPULATE)
+      .get(BASE_URL + ITEMS_URL + POPULATE)
       .then((response) => setData(response.data.data));
   }, []);
 
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Sugget</h1>
       <ul>
         {data.length > 0
           ? data.map((item, idx) => {
               return (
               <div className="league" key={idx}>
-                <div className="image">{item.attributes.image.url}</div>
-                <li>{item.attributes.name}</li>
-                <p>{item.attributes.description}</p>
+                
+               
+                <h3>{item.attributes.name}</h3>
+                <img className="image" src={item.attributes.image} />
+                <li>{item.attributes.description}</li>
+                <button>PURCHASE</button>
+                
               </div>
               )
             })
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export const BASE_URL = 'https://strapima3ca.herokuapp.com/api/';
-export const ANIMALS_URL = 'restaurants';
-export const POPULATE = '?populate=image';
+export const ITEMS_URL = 'restaurants';
+export const POPULATE = '?populate=*';
 
 
 export const getData = (url) => {
   axios.get(url).then((response) => console.log(response.data.data));
 };
 
-export const getAnimals = getData(BASE_URL + ANIMALS_URL + POPULATE);
+export const getAnimals = getData(BASE_URL + ITEMS_URL + POPULATE);
